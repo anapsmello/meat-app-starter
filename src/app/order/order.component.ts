5ffbe2a0697842e7cc3f8210969f27d728e03150
Ana Paula Santos de Mello <aninha.bcc@gmail.com>
2017-09-10 15:30:56 -0300

    aula 65 - fornecendo itens de compra

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {RadioOption} from '../shared/radio/radio-option.model'
 
+import {OrderService} from './order.service'
+import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
+
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
@@ -10,12 +13,27 @@ export class OrderComponent implements OnInit {
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
     {label: 'Cartão de Débito', value: 'DEB'},
-    {label: 'Cartão Refeição', value: 'REF'}    
+    {label: 'Cartão Refeição', value: 'REF'}
   ]
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
   }
 
+  cartItems(): CartItem[] {
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item)
+  }
 }

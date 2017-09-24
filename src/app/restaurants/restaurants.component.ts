a49cfec7870b0b7ae4abf3983bd20880dcd9334c
Ana Paula Santos de Mello <aninha.bcc@gmail.com>
2017-09-24 19:54:51 -0300

    Aula 105: Prevenindo a Interrupção do Observable ValueChanges

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -6,6 +6,9 @@ import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import {Observable} from 'rxjs/Observable'
 
 import {Restaurant} from './restaurant/restaurant.model'
 import {RestaurantsService} from './restaurants.service'
@@ -47,7 +50,9 @@ export class RestaurantsComponent implements OnInit {
     this.searchControl.valueChanges
     .debounceTime(500) // só faz a busca após 500ms entre os eventos
     .distinctUntilChanged() // só pesquisa se mudar a string
-    .switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm)).subscribe(restaurants => this.restaurants = restaurants) //switchMap faz o unsubscribe do evento anterior (se demorasse entre uma req e outra, não teria problema)
+    .switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm)
+    .catch(error => Observable.from([])))
+    .subscribe(restaurants => this.restaurants = restaurants) //switchMap faz o unsubscribe do evento anterior (se demorasse entre uma req e outra, não teria problema)
 
     this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
     }
